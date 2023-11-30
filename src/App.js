e771ff79f25e9073ba1213cd57b27aac82c38bb1
andbudha <andreibartov@gmail.com>
2023-11-30 21:58:28 +0100

    PizzaSkeleton comp created and updated

--- src/App.js
+++ src/App.js
@@ -6,24 +6,20 @@ import { Categories } from './components/Categories/Categories';
 import { Sort } from './components/Sort/Sort';
 import { useEffect, useState } from 'react';
 import { PizzaCard } from './components/PizzaCard/PizzaCard';
+import { PizzaSkeleton } from './components/Skeletons/PizzaSkeleton';
 
 function App() {
   const [pizzas, setPizzas] = useState([]);
 
   useEffect(() => {
     fetch('https://656897589927836bd975198a.mockapi.io/reactpizza/api/1/items')
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => {
-        setPizzas(data);
-        return data;
-      });
+      .then((res) => res.json())
+      .then((data) => setPizzas(data));
   }, []);
 
   const pizzaList = pizzas.map((pizza) => {
     return (
-      <PizzaCard
+      <PizzaSkeleton
         key={pizza.id}
         pizzaname={pizza.name}
         pizzaImage={pizza.imageUrl}
---
+++ src/components/Skeletons/PizzaSkeleton.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import ContentLoader from 'react-content-loader';
+
+export const PizzaSkeleton = (props) => (
+  <ContentLoader
+    className="pizza-block"
+    speed={2}
+    width={280}
+    height={466}
+    viewBox="0 0 280 466"
+    backgroundColor="#f7f7f7"
+    foregroundColor="#fdfaff"
+    {...props}
+  >
+    <circle cx="137" cy="138" r="110" />
+    <rect x="0" y="266" rx="10" ry="10" width="278" height="27" />
+    <rect x="2" y="400" rx="5" ry="5" width="50" height="44" />
+    <rect x="173" y="399" rx="20" ry="20" width="104" height="44" />
+    <rect x="190" y="410" rx="0" ry="0" width="4" height="10" />
+    <rect x="0" y="306" rx="10" ry="10" width="278" height="86" />
+  </ContentLoader>
+);
