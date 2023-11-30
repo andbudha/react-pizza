d12e6574c40ea9845597ca067cafa2e34e39b396
andbudha <andreibartov@gmail.com>
2023-11-30 10:25:37 +0100

    categories comp updated/categories mapped

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -1,13 +1,33 @@
-export const Categories = (props) => {
+import * as React from 'react';
+
+export const Categories = () => {
+  const categories = [
+    'Все',
+    'Мясные',
+    'Вегетарианская',
+    'Гриль',
+    'Острые',
+    'Закрытые',
+  ];
+  const [activeIndex, setActiveIndex] = React.useState(0);
+
+  const setIndexHandler = (index) => {
+    setActiveIndex(index);
+  };
   return (
     <div className="categories">
       <ul>
-        <li className="active">Все</li>
-        <li>Мясные</li>
-        <li>Вегетарианская</li>
-        <li>Гриль</li>
-        <li>Острые</li>
-        <li>Закрытые</li>
+        {categories.map((cat, index) => {
+          return (
+            <li
+              key={index}
+              className={activeIndex === index ? 'active' : ''}
+              onClick={() => setIndexHandler(index)}
+            >
+              {cat}
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
