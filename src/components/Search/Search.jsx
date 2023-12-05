2db899100674a0455e89f7a7b90a132745ef97a0
andbudha <andreibartov@gmail.com>
2023-12-05 10:04:05 +0100

    item search input autofocusing inplemented

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -5,21 +5,25 @@ import { AppContext } from '../../App';
 
 export const Search = () => {
   const { searchValue, setSearchValue } = React.useContext(AppContext);
+  const inputRef = React.useRef();
+  const clearInputHandler = () => {
+    setSearchValue('');
+    inputRef.current.focus();
+  };
   return (
     <div className={styles.search_box}>
       <IoIosSearch className={styles.search_icon} />
       <input
+        ref={inputRef}
         value={searchValue}
+        autoFocus
         onChange={(event) => setSearchValue(event.currentTarget.value)}
         className={styles.search_input}
         type="text"
         placeholder="Search..."
       />
       {searchValue && (
-        <IoMdClose
-          className={styles.close_icon}
-          onClick={() => setSearchValue('')}
-        />
+        <IoMdClose className={styles.close_icon} onClick={clearInputHandler} />
       )}
     </div>
   );
