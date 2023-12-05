ce7f0adfb706398dd721f68b520a3543907d694b
andbudha <andreibartov@gmail.com>
2023-12-05 12:54:52 +0100

    debounce from lodash applied

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -2,22 +2,33 @@ import * as React from 'react';
 import styles from './Search.module.css';
 import { IoIosSearch, IoMdClose } from 'react-icons/io';
 import { AppContext } from '../../App';
+import debounce from 'lodash.debounce';
 
 export const Search = () => {
   const { searchValue, setSearchValue } = React.useContext(AppContext);
+  const [localInputValue, setLocalInputValue] = React.useState('');
   const inputRef = React.useRef();
   const clearInputHandler = () => {
     setSearchValue('');
     inputRef.current.focus();
+  };
+
+  const delaySettingSearchValue = React.useCallback(
+    debounce((str) => setSearchValue(str), 500),
+    []
+  );
+  const setSearchValueHandler = (event) => {
+    setLocalInputValue(event.currentTarget.value);
+    delaySettingSearchValue(event.currentTarget.value);
   };
   return (
     <div className={styles.search_box}>
       <IoIosSearch className={styles.search_icon} />
       <input
         ref={inputRef}
-        value={searchValue}
+        value={localInputValue}
         autoFocus
-        onChange={(event) => setSearchValue(event.currentTarget.value)}
+        onChange={setSearchValueHandler}
         className={styles.search_input}
         type="text"
         placeholder="Search..."
