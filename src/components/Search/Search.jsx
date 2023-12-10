10d420a65c5a223670a48a616490386462c5a279
andbudha <andreibartov@gmail.com>
2023-12-10 11:43:10 +0100

    clearinf search input updated

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -7,12 +7,14 @@ import { useDispatch, useSelector } from 'react-redux';
 import { setSearchValue } from '../../redux/slices/filterSlice';
 
 export const Search = () => {
-  const searchValue = useSelector(searchValueSelector);
-  const dispatch = useDispatch();
   const [localInputValue, setLocalInputValue] = React.useState('');
   const inputRef = React.useRef();
+  const searchValue = useSelector(searchValueSelector);
+  const dispatch = useDispatch();
+
   const clearInputHandler = () => {
     dispatch(setSearchValue(''));
+    setLocalInputValue('');
     inputRef.current.focus();
   };
 
