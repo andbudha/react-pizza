5be5eae4d0f5a1cde777ffbbd6ca98de6a9ef8f8
andbudha <andreibartov@gmail.com>
2023-12-13 11:55:43 +0100

    Search updated/ts rules applied

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -3,26 +3,29 @@ import styles from './Search.module.css';
 import { IoIosSearch, IoMdClose } from 'react-icons/io';
 import debounce from 'lodash.debounce';
 import { searchValueSelector } from '../../redux/Selectors/Selectors';
-import { useDispatch, useSelector } from 'react-redux';
-import { setSearchValue } from '../../redux/slices/filterSlice';
+import { useSelector } from 'react-redux';
+import { setSearchValue } from '../../redux/slices/filterSlice.ts';
+import { AppRootState, useAppDispatch } from '../../redux/store.tsx';
 
 export const Search = () => {
   const [localInputValue, setLocalInputValue] = React.useState('');
-  const inputRef = React.useRef();
-  const searchValue = useSelector(searchValueSelector);
-  const dispatch = useDispatch();
+  const inputRef = React.useRef<HTMLInputElement>(null);
+  const searchValue = useSelector<AppRootState, string>(searchValueSelector);
+  const dispatch = useAppDispatch();
 
   const clearInputHandler = () => {
-    dispatch(setSearchValue(''));
+    dispatch(setSearchValue({ value: '' }));
     setLocalInputValue('');
-    inputRef.current.focus();
+    inputRef.current?.focus();
   };
 
   const delaySettingSearchValue = React.useCallback(
-    debounce((str) => dispatch(setSearchValue(str)), 500),
+    debounce((str: string) => dispatch(setSearchValue({ value: str })), 500),
     []
   );
-  const setSearchValueHandler = (event) => {
+  const setSearchValueHandler = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
     setLocalInputValue(event.currentTarget.value);
     delaySettingSearchValue(event.currentTarget.value);
   };
